685d35dfa7e190b084ea544c00065bd154346553
jeecko <aldrinthomaslab@gmail.com>
2017-06-09 18:12:16 +0800

    remove unnessary files and fixed configs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+
+const App = () => (<h1>hello</h1>);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
