d63a923c64f89269d22c7275f0370b148110df4d
Gerald Agustin <gerald.agustin09@gmail.com>
2017-06-18 04:00:14 +0800

    anime staggering in content window nav

--- src/components/VerticalNav/VerticalNav.js
+++ src/components/VerticalNav/VerticalNav.js
@@ -44,31 +44,35 @@ class VerticalNav extends React.Component {
         <div className={`wholeview-navigation${(isNavWindowOpen ? ' show' : '')}`}>
           <div className="halfview left">
             <div className="core-contact">
-              <h5><i className="ion-ios-arrow-forward" />Contact</h5>
-              <h3>gerald.agustin09@gmail.com</h3>
-              <ul className="micro-navsocial">
-                <li><a href="https://www.facebook.com/iamGeraldAgustin">facebook</a></li>
-                <li><a href="https://twitter.com/graldagstin">twitter</a></li>
-                <li><a href="https://www.instagram.com/agustin.gerald/">instagram</a></li>
-                <li><a href="https://dribbble.com/grldagstin">dribbble</a></li>
-              </ul>
+              <Anime easing="easeInOutQuad" loop={false} duration={300} delay={(el, index) => index * 130} opacity={(isNavWindowOpen ? 1 : 0)} translateY={(isNavWindowOpen ? '0' : '150%')}>
+                <h5><i className="ion-ios-arrow-forward" />Contact</h5>
+                <h3>gerald.agustin09@gmail.com</h3>
+                <ul className="micro-navsocial">
+                  <li><a href="https://www.facebook.com/iamGeraldAgustin">facebook</a></li>
+                  <li><a href="https://twitter.com/graldagstin">twitter</a></li>
+                  <li><a href="https://www.instagram.com/agustin.gerald/">instagram</a></li>
+                  <li><a href="https://dribbble.com/grldagstin">dribbble</a></li>
+                </ul>
+              </Anime>
             </div>
           </div>
           <div className="halfview right">
             <nav className="core-navigation">
-              <h5><i className="ion-ios-arrow-forward" />Navigation</h5>
-              <div className="core-links">
-                <h3>About</h3>
-              </div>
-              <div className="core-links">
-                <h3>Vision</h3>
-              </div>
-              <div className="core-links">
-                <h3>Studio</h3>
-              </div>
-              <div className="core-links">
-                <h3>Blog</h3>
-              </div>
+              <Anime easing="easeInOutQuad" loop={false} duration={300} delay={(el, index) => index * 90} opacity={(isNavWindowOpen ? 1 : 0)} translateY={(isNavWindowOpen ? '0' : '150%')}>
+                <h5><i className="ion-ios-arrow-forward" />Navigation</h5>
+                <div className="core-links">
+                  <h3>About</h3>
+                </div>
+                <div className="core-links">
+                  <h3>Vision</h3>
+                </div>
+                <div className="core-links">
+                  <h3>Studio</h3>
+                </div>
+                <div className="core-links">
+                  <h3>Blog</h3>
+                </div>
+              </Anime>
             </nav>
           </div>
         </div>
